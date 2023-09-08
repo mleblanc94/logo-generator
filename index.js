3bf0ac9f7aea71eb51c8780c6003cc16efaad3c0
Michael Leblanc <mleblanc1@werfen.com>
2023-09-08 15:09:28 -0400

    added a test to ensure that users only enter 3 characters

--- index.js
+++ index.js
@@ -50,7 +50,14 @@ inquirer
     {
         type: 'input',
         name: 'text',
-        message: 'Please enter 3 characters of text to be included in your new logo:'
+        message: 'Please enter 3 characters of text to be included in your new logo:',
+        validate: function (input) {
+            if (input.length === 3) {
+                return true;
+            } else {
+                return "Please enter 3 characters for your logo"
+            }
+        }
     },
     {
         type: 'input',
